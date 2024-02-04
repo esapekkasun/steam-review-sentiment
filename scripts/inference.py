ece09ed012c4099dfe678558d21b9f221a4c06b5
Esa-Pekka Sundell <eppu@avidaya.com>
2024-02-04 02:09:52 +0200

    Modified inference file to contain model pipeline function

--- scripts/inference.py
+++ scripts/inference.py
@@ -4,10 +4,7 @@
 This script is used for testing the model inference manually
 """
 
-import pandas as pd
 import torch
-from torch import nn
-from tqdm import tqdm
 from train_rnn_model import RNN, batch_to_tensors, accuracy_fn,\
                             embedding_size, hidden_size, num_layers, \
                             device, batch_size, model_save_path
@@ -19,20 +16,26 @@ filename_test = "../data/reviews_test.csv"
 model = RNN(embedding_size, hidden_size, num_layers).to(device)
 model.load_state_dict(torch.load(model_save_path))
 
-# Model testing
-while True:
-    sentence = input("Input (write 'quit' to end):")
-    if sentence == "quit":
-        break
-    
-    print(f"\n> {sentence}")
+# Define model pipeline
+def model_pipeline(text: str):
     with torch.no_grad():
-        X, Y = batch_to_tensors([sentence], [0])
+        X, Y = batch_to_tensors([text], [0])
         
         # Do forward pass and predict
         output = model(X)
         predictions = torch.round(torch.sigmoid(output))
-
-        # Print prediction
         guess = "Voted up" if predictions[0] else "Voted down"
+        return guess
+
+if __name__ == "__main__":
+
+    # Model testing
+    while True:
+        text = input("Input (write 'quit' to end):")
+        if text == "quit":
+            break
+
+        # Print text and prediction
+        print(f"\n> {text}")
+        guess = model_pipeline(text)
         print(guess, "\n")
