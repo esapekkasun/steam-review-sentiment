b7463be3849d48dfbb684c11ae1b671c03bae5b3
Esa-Pekka Sundell <eppu@avidaya.com>
2024-02-04 02:15:28 +0200

    Added script for generating inference API

---
+++ scripts/main.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+""" 
+This script runs the model API
+"""
+
+from fastapi import FastAPI
+from inference import model_pipeline
+
+# Initialise app
+app = FastAPI()
+
+# Implement GET response
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+# Implement POST response
+@app.post("/review")
+def ask(text: str):
+    sentiment = model_pipeline(text)
+    return {"sentiment": sentiment}
